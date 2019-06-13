24ec4df5b0657b90b09d2980a802f91a3297a53c
CLux <cplux7@gmail.com>
2019-06-13 11:18:34 -0400

    created home index and have working test for home controller

---
+++ reviewsite.Tests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using reviewsite.Controllers;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace reviewsite.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_Hello_World()
+        {
+            var underTest = new HomeController();
+
+            var result = underTest.Index();
+
+            Assert.Equal("Hello world!", result);
+        }
+    }
+}
