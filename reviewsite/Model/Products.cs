bd4294af38de8da607c0c2da216c527cbcf40a70
CLux <cplux7@gmail.com>
2019-06-14 14:14:07 -0400

    started changes to get page working

--- reviewsite/Model/Products.cs
+++ reviewsite/Model/Products.cs
@@ -9,17 +9,21 @@ namespace reviewsite.Model
     {
         public class Product
         {
-            public int Id;
-            public string Name;
-            public string Category;
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
 
 
-            public Product(int id, string name, string category)
+            public Product(int id, string name, string description, string category)
             {
                 Id = id;
                 Name = name;
+                Description = Description;
                 Category = category;
             }
+
+
         }
 
         Dictionary <int, string> products;
