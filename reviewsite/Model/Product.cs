3974273e030dc67f27b09d45f63531bab0c14a90
Angel-TheDev <angelantoinetted@yahoo.com>
2019-06-21 15:32:48 -0400

    Deleted Home controller, Product model - WIP, Review model- no longer in use, will need to update database with new seed data

--- reviewsite/Model/Product.cs
+++ reviewsite/Model/Product.cs
@@ -9,18 +9,18 @@ namespace reviewsite.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Category { get; set; }
+        public string Review { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
         public int CategoryId { get; set; }
 
-        public Review Review { get; set; }
+        public Category Category { get; set; }
 
-        public Product(int id, string name, string category, string description, string image)
+        public Product(int id, string name, string review, string description, string image)
         {
             Id = id;
             Name = name;
-            Category = category;
+            Review = review;
             Description = description;
             Image = image;
            
