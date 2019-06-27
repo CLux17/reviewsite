b2542640fb7e7338ce5bda4a10fdc5e22d39b6c8
CLux <cplux7@gmail.com>
2019-06-27 10:31:50 -0400

    added in generic tests to use as framework for our projects tests

--- reviewsite.Tests/HomeControllerTests.cs
+++ reviewsite.Tests/HomeControllerTests.cs
@@ -32,5 +32,54 @@ namespace reviewsite.Tests
 
 
         }
-    }
+
+        public class CourseControllerTests
+        {
+            CourseController underTest;
+            IRepository<Course> courseRepo;
+
+            public CourseControllerTests()
+            {
+                courseRepo = Substitute.For<IRepository<Course>>();
+                underTest = new CourseController(courseRepo);
+            }
+
+            [Fact]
+            public void Index_Returns_A_View()
+            {
+                var result = underTest.Index();
+
+                Assert.IsType<ViewResult>(result);
+            }
+
+            [Fact]
+            public void Index_Passes_All_Courses_To_View()
+            {
+                var expectedCourses = new List<Course>();
+                courseRepo.GetAll().Returns(expectedCourses);
+
+                var result = underTest.Index();
+
+                Assert.Equal(expectedCourses, result.Model);
+            }
+
+            [Fact]
+            public void Details_Returns_A_View()
+            {
+                var result = underTest.Details(1);
+
+                Assert.IsType<ViewResult>(result);
+            }
+
+            [Fact]
+            public void Details_Passes_Course_To_View()
+            {
+                var expectedCourse = new Course();
+                courseRepo.GetById(1).Returns(expectedCourse);
+
+                var result = underTest.Details(1);
+
+                Assert.Equal(expectedCourse, result.Model);
+            }
+        }
 }
---
+++ reviewsite.Tests/ProductTests.cs
@@ -0,0 +1,46 @@
+﻿using reviewsite.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace reviewsite.Tests
+{
+    public class ProductTests
+    {
+        
+            Product underTest;
+
+            public ProductTests()
+            {
+                underTest = new Product(42, "Course Name", "Description of course");
+            }
+
+            [Fact]
+            public void ProductConstructor_Sets_Name_On_CourseModel()
+            {
+                var result = underTest.Name;
+
+                Assert.Equal("Course Name", result);
+            }
+
+            [Fact]
+            public void CourseConstructor_Sets_Id_On_CourseModel()
+            {
+                var result = underTest.Id;
+
+                Assert.Equal(42, result);
+            }
+
+            [Fact]
+            public void CourseConstructor_Sets_Description_On_CourseModel()
+            {
+                var result = underTest.Description;
+
+                Assert.Equal("Description of course", result);
+            }
+
+        
+
+    }
+}
