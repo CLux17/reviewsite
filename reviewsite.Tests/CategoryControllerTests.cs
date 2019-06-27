3d9f09e87732565fcb520769da8c2cf164951846
Angel-TheDev <angelantoinetted@yahoo.com>
2019-06-27 11:28:52 -0400

    Readded NSub method and added NSub Nuget- CategoryControllerTests running

--- reviewsite.Tests/CategoryControllerTests.cs
+++ reviewsite.Tests/CategoryControllerTests.cs
@@ -3,6 +3,7 @@ using reviewsite.Controllers;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using NSubstitute;
 using Microsoft.AspNetCore.Mvc;
 using reviewsite.Repositories;
 using reviewsite.Model;
@@ -11,8 +12,15 @@ namespace reviewsite.Tests
 {
     public class CategoryControllerTests
     {
-            CategoryController underTest;
-            IRepository<Category> categoryRepo;
+        CategoryController underTest;
+        IRepository<Category> categoryRepo;
+
+        public CategoryControllerTests()
+        {
+            categoryRepo = Substitute.For<IRepository<Category>>();
+            underTest = new CategoryController(categoryRepo);
+        }
+
         [Fact]
         public void Index_Returns_A_View()
         {
@@ -27,35 +35,32 @@ namespace reviewsite.Tests
             var result = underTest.Details(1);
 
             Assert.IsType<ViewResult>(result);
-
-
-
-
         }
 
 
-            [Fact]
-            public void Index_Passes_All_Category_To_View()
-            {
-                var expectedCategories = new List<Category>();
-                categoryRepo.GetAll().Returns(expectedCategories);
+        [Fact]
+        public void Index_Passes_All_Category_To_View()
+        {
+            var expectedCategories = new List<Category>();
+            categoryRepo.GetAll().Returns(expectedCategories);
 
-                var result = underTest.Index();
+            var result = underTest.Index();
 
-                Assert.Equal(expectedCategories, result.Model);
-            }
-
-           
-
-            [Fact]
-            public void Details_Passes_Category_To_View()
-            {
-                var expectedCategory = new Category();
-                categoryRepo.GetById(1).return (expectedCategory);
-
-                var result = underTest.Details(1);
-
-                Assert.Equal(expectedCategory, result.Model);
-            }
+            Assert.Equal(expectedCategories, result.Model);
         }
+
+
+
+        [Fact]
+        public void Details_Passes_Category_To_View()
+        {
+            var expectedCategory = new Category();
+            categoryRepo.GetById(1).Returns(expectedCategory);
+
+            var result = underTest.Details(1);
+
+            Assert.Equal(expectedCategory, result.Model);
+        }
+    }
+}
 
